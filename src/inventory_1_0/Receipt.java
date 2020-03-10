4ce5a417df7c6367675ce129dbd0dd8e1c3629a0
Tharinda Dilshan Piyadasa <tharindad7@gmail.com>
2020-03-10 23:09:58 +0530

    Initial commit

---
+++ src/inventory_1_0/Receipt.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package inventory_1_0;
+
+public class Receipt {
+    private int id;
+    private String receiptNo;
+    private String type;
+    private String date;
+    private byte[] picture;
+    
+//    Constructor
+    public Receipt(int rId, String rNo, String rType, String rDate, byte[] rImg){
+        this.id = rId;
+        this.receiptNo = rNo;
+        this.type = rType;
+        this.date = rDate;
+        this.picture = rImg;
+    }
+}
